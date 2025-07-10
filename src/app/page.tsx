1314ccbf436d8e1132158a89d514a758500ad6e1
Nweke-Justin <nwekejustin580@gmail.com>
2025-07-10 12:22:44 -0700

    i added a few functionalities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Header from "./Components/Header";
 import Image from 'next/image'
 import img1 from "../../public/pictures/img1.png"
@@ -19,54 +20,58 @@ import img19 from "../../public/pictures/img19.png"
 import img20 from "../../public/pictures/img20.png"
 import img23 from "../../public/pictures/img23.png"
 import { X } from 'lucide-react';
-
+import { useState } from "react";
 import Observer from "./Components/Observer";
 import Footer from "./Components/Footer";
 export default function Home() {
+  const [showCard1, setShowCard1] = useState<boolean>(false);
+  const [showCard2, setShowCard2] = useState<boolean>(false);
+  const [showCard3, setShowCard3] = useState<boolean>(false);
+  const [showCard4, setShowCard4] = useState<boolean>(false);
   return (
     <div className=" ">
       <div className="bg-[#EDF1FF]">
-      <Header />
-      {/* desktop view,1st section */}
-      <div className="hidden lg:flex space-x-[50px] mt-[52px] xl:space-x-[103px]">
-        <div className="lg:ml-[50px] xl:ml-[150px] lg:w-[50%] xl:w-[520px] ">
-          <div className="space-y-[16px] mb-[44px] ">
-            <h1 className=" font-bold lg:text-[30px] xl:text-[40px]">Revolutionize Currency Exchange Transactions across Africa</h1>
-            <p className="text-[#565656] text-[20px] font-bold">Offering instant, secure, and affordable currency exchange across Africa.</p>
-          </div>
-          <div className="flex space-x-[20px] ">
-            <div className=" w-[202.5px] h-[60px]">
-              <Image src={img1} alt="" width={0} height={0} className=" w-full" />
+        <Header />
+        {/* desktop view,1st section */}
+        <div className="hidden lg:flex space-x-[50px] mt-[52px] xl:space-x-[103px]">
+          <div className="lg:ml-[50px] xl:ml-[150px] lg:w-[50%] xl:w-[520px] ">
+            <div className="space-y-[16px] mb-[44px] ">
+              <h1 className=" font-bold lg:text-[30px] xl:text-[40px]">Revolutionize Currency Exchange Transactions across Africa</h1>
+              <p className="text-[#565656] text-[20px] font-bold">Offering instant, secure, and affordable currency exchange across Africa.</p>
             </div>
-            <div className=" w-[202.5px] h-[60px]">
-              <Image src={img2} alt="" width={0} height={0} className=" w-full" />
+            <div className="flex space-x-[20px] ">
+              <div className=" w-[202.5px] h-[60px]">
+                <Image src={img1} alt="" width={0} height={0} className=" w-full" />
+              </div>
+              <div className=" w-[202.5px] h-[60px]">
+                <Image src={img2} alt="" width={0} height={0} className=" w-full" />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-[12px]  ">
-          <Image src={img3} alt="your browser doesnt support image" width={0} height={0} className="w-[596px] lg:h-[450px] xl:h-[514px]" />
-        </div>
-      
-      </div>
-      {/* mobile view /tablet 1 section*/}
-      <div className="lg:hidden bg-[#EDF1FF]">
-        <div className="w-[95vw] mx-auto space-x-[10.55px] mt-[36px] mb-[20px]">
-          <h1 className="font-semibold text-[26px] md:text-[35px]">Revolutionize Currency Exchange Transactions across Africa
-          </h1>
-          <p className=" text-12px font-semibold text-[#565656]">Offering instant, secure, and affordable currency exchange across Africa.</p>
-        </div>
-        <div className="flex space-x-[13.33px] w-[91vw] mx-auto mb-[32px] ]">
-          <div className="w-[135px] md:w-[202.5px] h-[40px] md:h-[60px]">
-            <Image src={img1} alt="" width={0} height={0} className="w-full h-full" />
+          <div className="mt-[12px]  ">
+            <Image src={img3} alt="your browser doesnt support image" width={0} height={0} className="w-[596px] lg:h-[450px] xl:h-[514px]" />
           </div>
-          <div className="w-[135px] md:w-[202.5px] h-[40px] md:h-[60px]">
-            <Image src={img2} alt="" width={0} height={0} className="w-full h-full" />
+
+        </div>
+        {/* mobile view /tablet 1 section*/}
+        <div className="lg:hidden bg-[#EDF1FF]">
+          <div className="w-[95vw] mx-auto space-x-[10.55px] mt-[36px] mb-[20px]">
+            <h1 className="font-semibold text-[26px] md:text-[35px]">Revolutionize Currency Exchange Transactions across Africa
+            </h1>
+            <p className=" text-12px font-semibold text-[#565656]">Offering instant, secure, and affordable currency exchange across Africa.</p>
+          </div>
+          <div className="flex space-x-[13.33px] w-[91vw] mx-auto mb-[32px] ]">
+            <div className="w-[135px] md:w-[202.5px] h-[40px] md:h-[60px]">
+              <Image src={img1} alt="" width={0} height={0} className="w-full h-full" />
+            </div>
+            <div className="w-[135px] md:w-[202.5px] h-[40px] md:h-[60px]">
+              <Image src={img2} alt="" width={0} height={0} className="w-full h-full" />
+            </div>
+          </div>
+          <div className="w-[91vw] mx-auto">
+            <Image src={img3} alt="" width={0} height={0} />
           </div>
         </div>
-        <div className="w-[91vw] mx-auto">
-          <Image src={img3} alt="" width={0} height={0} />
-        </div>
-      </div>
       </div>
       {/* desktop view section2 */}
       <div className="w-full bg-[#1E1E1E] py-[46px] hidden lg:block">
@@ -266,24 +271,64 @@ export default function Home() {
           Frequently asked questions <br className="lg:hidden" /> (FAQs)
         </h1>
         <div className="space-y-[12px]">
+
           <div className="w-[95vw] lg:w-[79vw] mx-auto bg-white rounded-lg p-[40px]">
             <div className="w-[99%] lg:w-[90%] mx-auto justify-between flex ">
-              <h1 className="font-semibold text-[15px] md:text-[20px]">What services does Transwire offer</h1> <X className="rounded-full p-2 bg-gray-100" width={35} height={35} />
+              <div>
+                <h1 className="font-semibold text-[15px] md:text-[20px] mb-[18px]">What services does Transwire offer</h1>
+                {showCard1 && (
+                  <p className="text-[#A5A5A5]">Transwire specializes in seamless international money transfers, currency exchange, and cross-border payments. Whether you need to send money abroad, pay for services, or handle business transactionsTranswire specializes in seamless international money transfers, currency exchange, and cross-border payments. Whether you need to send money abroad, pay for services, or handle business transactions</p>)}
+              </div>
+              <div>
+              <X className="rounded-full p-2 bg-gray-100 hover:cursor-pointer" width={35} height={35} onClick={() => setShowCard1(prev => !prev)}
+              />
+              </div>
             </div>
           </div>
-         <div className="w-[95vw]  lg:w-[79vw] mx-auto bg-white rounded-lg p-[40px]">
+          <div className="w-[95vw]  lg:w-[79vw] mx-auto bg-white rounded-lg p-[40px]">
             <div className="w-[99%] lg:w-[90%]  mx-auto justify-between flex ">
-              <h1 className="font-semibold text-[15px] md:text-[20px]">What services does Transwire offer</h1> <X className="rounded-full p-2 bg-gray-100" width={35} height={35} />
+              <div>
+                <h1 className="font-semibold text-[15px] md:text-[20px] mb-[18px]">How long does it take for a transaction to be completed?</h1>
+                {showCard2 && (
+                  <p className="text-[#A5A5A5]">Transaction times may vary depending on the destination country and payment method. However, most transfers are completed within 1–3 business days. You can track your transfer in real time through the Transwire app or website.</p>
+                )}
+
+              </div>
+              <div>
+                <X className="rounded-full p-2 bg-gray-100 hover:cursor-pointer" width={35} height={35} onClick={() => setShowCard2(prev => !prev)} />
+              </div>
             </div>
           </div>
-         <div className="w-[95vw] lg:w-[79vw] mx-auto bg-white rounded-lg p-[40px]">
+          <div className="w-[95vw] lg:w-[79vw] mx-auto bg-white rounded-lg p-[40px]">
             <div className="w-[99%] lg:w-[90%]  mx-auto justify-between flex ">
-              <h1 className="font-semibold text-[15px] md:text-[20px]">What services does Transwire offer</h1> <X className="rounded-full p-2 bg-gray-100" width={35} height={35} />
+              <div>
+                <h1 className="font-semibold text-[15px] md:text-[20px] mb-[18px]">What services does Transwire offer</h1>
+                {showCard3 && (
+
+                  <p className="text-[#A5A5A5]">
+                    Yes, Transwire charges a small fee for each transaction, which is displayed upfront before you confirm the transfer. We pride ourselves on transparency—there are no hidden costs, and our fees are among the lowest in the industry.
+                  </p>
+                )}
+
+              </div>
+              <div>
+                <X className="rounded-full p-2 bg-gray-100 hover:cursor-pointer" width={35} height={35} onClick={() => setShowCard3(prev => !prev)} />
+              </div>
             </div>
           </div>
-         <div className="w-[95vw] lg:w-[79vw] mx-auto bg-white rounded-lg p-[40px]">
+          <div className="w-[95vw] lg:w-[79vw] mx-auto bg-white rounded-lg p-[40px]">
             <div className="w-[99%] lg:w-[90%] mx-auto justify-between flex ">
-              <h1 className="font-semibold text-[15px] md:text-[20px]">What services does Transwire offer</h1> <X className="rounded-full p-2 bg-gray-100" width={35} height={35} />
+              <div>
+                <h1 className="font-semibold text-[15px] md:text-[20px] mb-[18px]">What services does Transwire offer</h1>
+                {showCard4 && (
+                  <p className="text-[#A5A5A5]">
+                    Absolutely! Transwire uses state-of-the-art security measures, including encryption and multi-factor authentication, to ensure that your money and personal information are always protected. We are a fully licensed financial services provider, and thousands of customers trust us with their foreign transactions every day.
+                  </p>
+                )}
+              </div>
+              <div>
+                <X className="rounded-full p-2 bg-gray-100 hover:cursor-pointer" width={35} height={35} onClick={() => setShowCard4(prev => !prev)} />
+              </div>
             </div>
           </div>
         </div>
@@ -324,13 +369,13 @@ export default function Home() {
             <p className="text-[#A5A5A5] font-medium text-[12px] md:text-[16px] mb-[20px] lg:mb-[28px] text-center">Subscribe to our newsletter and be the first to know about updates.</p>
             <div className="w-[90vw] md:w-[60vw] lg:w-[44vw] mx-auto flex mb-[10px] lg:mb-[92px] bg-[#F3F3F3] pl-[18px] lg:pl-[24px]">
               <input type="text" placeholder="Enter email address" className="w-full font-bold text-black" />
-              <button className="px-[18px] lg:px-[24px] py-[12px] lg:py-[16px] text-white bg-[#2E42A5] rounded-lg lg:rounded-none lg:rounded-tr-lg lg:rounded-br-lg text-[10.5px] md:text-[14px] font-medium">Subscribe</button>
+              <button className="px-[18px] lg:px-[24px] py-[12px] lg:py-[16px] text-white bg-[#2E42A5] rounded-tr-lg rounded-br-lg text-[10.5px] md:text-[14px] font-medium">Subscribe</button>
             </div>
           </div>
         </div>
       </div>
       <div className="bg-[#F3F3F3] pt-[46px]">
-      <Footer/>
+        <Footer />
       </div>
     </div>
   );
